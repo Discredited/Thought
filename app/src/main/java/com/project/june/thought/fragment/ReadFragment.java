543da1056cd29d82d9b08861916cd00e9ab8ed17
June <214839412@qq.com>
2017-03-25 09:32:35 +0800

    readFragment create

---
+++ app/src/main/java/com/project/june/thought/fragment/ReadFragment.java
@@ -0,0 +1,15 @@
+package com.project.june.thought.fragment;
+
+import com.project.june.thought.base.BaseFragment;
+
+/**
+ * 阅读
+ * Created by June on 2017/3/25.
+ */
+
+public class ReadFragment extends BaseFragment {
+    @Override
+    protected int getContentViewId() {
+        return 0;
+    }
+}
