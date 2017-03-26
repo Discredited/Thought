b580d40f1c92f9646ca639da428bdfa3d846f484
June <214839412@qq.com>
2017-03-26 23:18:06 +0800

    阅读详情页面

---
+++ app/src/main/java/com/project/june/thought/activity/detail/ReadingDetailActivity.java
@@ -0,0 +1,23 @@
+package com.project.june.thought.activity.detail;
+
+import com.project.june.thought.R;
+import com.project.june.thought.base.BaseActivity;
+
+public class ReadingDetailActivity extends BaseActivity {
+
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_reading_detail;
+    }
+
+    @Override
+    protected int getTempFragmentId() {
+        return 0;
+    }
+
+    @Override
+    protected void logicProgress() {
+
+    }
+}
